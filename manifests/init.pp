6085868d17efb3f57bbc93909862d15debd2efc3
Alexey Shcherbakov <alexey.shcherbakov@kaspersky.com>
2020-11-09 21:04:57 +0300

    .

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,14 +36,21 @@ define tasks::set_settings_task {
 class kesl (
 
   $package_name = 'kesl',
+  $packageagent_name = 'klnagent64',
   $ensure = 'latest',
+  $ensureagent = 'latest',
   $service_name = 'kesl',
   $service_run = true,
   $service_enable = true,
+  $serviceagent_name = "${packageagent_name}",
+  $serviceagent_run = true,
+  $serviceagent_enable = true,
+  $agentconfpath = "/opt/kaspersky/${packageagent_name}/kesl_agent.ini",
   $confpath = '/opt/kaspersky/kesl/kesl.ini',
   $fullconfpath = '/opt/kaspersky/kesl/kesl_full.ini',
   $customconfpath = '/opt/kaspersky/kesl/custom_kesl.ini',
   $autoinstall = $::kesl::autoinstall,
+  $autoinstallagent = $::kesl::autoinstallagent,
   $customimports = $::kesl::customimports,
   $customtaskimports = $::kesl::customtaskimports,
   $customconfigversion = '11.1.0.3013',
@@ -101,4 +108,41 @@ class kesl (
     require => Package[$package_name],
     notify => Exec['save_full_config']
   }
+
+
+  package { $packageagent_name:
+  ensure => $ensureagent
+  }
+  ~>
+  if $ensureagent != 'absent' {
+  file { $agentconfpath:
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template('kesl/kesl_agent.ini.erb'),
+    notify  => Exec['init_agent_config']
+   }
+
+  exec { 'init_agent_config':
+    environment => ["KLAUTOANSWERS=${agentconfpath}"],
+    command => "/opt/kaspersky/${packageagent_name}/lib/bin/setup/postinstall.pl",
+    path    => ['/usr/bin', '/bin', '/usr/sbin'],
+    refreshonly => true,
+    notify => Exec['apply_agent_config']
+   }
+
+  exec { 'apply_agent_config':
+       command     => "/opt/kaspersky/${packageagent_name}/sbin/klnagent -reload-connectors",
+       path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
+       refreshonly => true
+   }
+
+  service { $serviceagent_name:
+    ensure => $serviceagent_run,
+    enable => $serviceagent_enable,
+    require => Package[$packageagent_name]
+   }
+  }
+
 }
